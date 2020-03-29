bf565a62383daa882c13006565a79e59e3a4aa9d
benhoumine <abdelkhalek.benhoumine@gmail.com>
2020-03-29 19:55:47 +0200

    test request post

--- app/src/main/java/com/example/greenpp/Activities/ActionActivity.java
+++ app/src/main/java/com/example/greenpp/Activities/ActionActivity.java
@@ -2,11 +2,23 @@ package com.example.greenpp.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.greenpp.Entities.Poubelle;
+import com.example.greenpp.Helper.Parameters;
 import com.example.greenpp.R;
 import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ActionActivity extends AppCompatActivity {
 
@@ -25,5 +37,39 @@ public class ActionActivity extends AppCompatActivity {
             this.poubelle.setCapacity(0.0);
             //Update capacity in bdd
         });
+    }
+
+
+    public void onClickButton(View view) {
+        System.out.println("helo clicked");
+        RequestQueue queue = Volley.newRequestQueue(ActionActivity.this);
+        String url = Parameters.URL_SERVER+Parameters.PORT+"/poubelles/testpost";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("error", String.valueOf(error));
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String> params = new HashMap<>();
+                params.put("id","1");
+                return params;
+            }
+        };
+        queue.add(postRequest);
     }
 }
--- app/src/main/java/com/example/greenpp/Helper/Parameters.java
+++ app/src/main/java/com/example/greenpp/Helper/Parameters.java
@@ -2,6 +2,6 @@ package com.example.greenpp.Helper;
 
 public class Parameters {
 
-    public static final String URL_SERVER="http://192.168.1.32:";
+    public static final String URL_SERVER="http://192.168.0.37:";
     public static final String PORT= "8081";
 }
